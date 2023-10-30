0a3ca89dd917047a36856b1615d32141ed2a4a5f
Jakob <jakob_pietrzyk@outlook.com>
2023-10-30 23:31:58 +0100

    Add forgot password feature with popup form

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -1,31 +1,46 @@
 <script>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 import { useAuthStore } from '@/stores/auth';
-
-const user = reactive({
-	email: '',
-	password: '',
-});
-
-function onSubmit() {
-	console.log(user);
-	if (user.email !== '' && user.password !== '') {
-		useAuthStore().login(user.email, user.password);
-	}
-}
+import ForgotPassword from './ForgotPassword.vue';
 
 export default {
 	name: 'LoginForm',
+	components: {
+		ForgotPassword
+	},
 	setup() {
+		const user = reactive({
+			email: '',
+			password: '',
+		});
+
+		function onSubmit() {
+			console.log(user);
+			if (user.email !== '' && user.password !== '') {
+				useAuthStore().login(user.email, user.password);
+			}
+		}
+
+		const showPopup = ref(false);
+
+		function showForgotPasswordPopup() {
+			showPopup.value = true;
+		}
+
+		function closePopup() {
+			showPopup.value = false;
+		}
+
 		return {
 			user,
 			onSubmit,
+			showPopup,
+			showForgotPasswordPopup,
+			closePopup,
 		};
 	},
 };
-
 </script>
-
 <template>
 	<div class="container">
 		<div class="login-form mx-auto">
@@ -41,11 +56,31 @@ export default {
 				<button type="submit" class="btn btn-primary">Logga in</button>
 			</form>
 		</div>
+
+		<div class="text-center mt-3">
+			<button @click="showForgotPasswordPopup" class="btn btn-primary">Glömt lösenord</button>
+		</div>
+
+		<transition name="fade">
+			<div v-if="showPopup" class="popup">
+				<ForgotPassword />
+				<button @click="closePopup" class="btn btn-primary">Stäng</button>
+			</div>
+		</transition>
 	</div>
 </template>
-
 <style scoped>
 .login-form {
 	width: 500px;
+}
+
+.popup {
+	position: fixed;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	background: white;
+	padding: 20px;
+	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 </style>

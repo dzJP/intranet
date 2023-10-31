dcd1fafb7b531714521896682b744f95a98adffb
Jakob <jakob_pietrzyk@outlook.com>
2023-10-31 02:25:11 +0100

    Implemented JavaMailSender for sending startup email notification

---
+++ backend/src/main/java/deltma/solutions/backend/services/EmailSenderService.java
@@ -0,0 +1,36 @@
+package deltma.solutions.backend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    // Method to send an email
+    public void sendEmail(String toEmail, String subject, String body) {
+        // Creating a new SimpleMailMessage object
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        // Setting the sender's email address
+        message.setFrom("jakobpietrzyk93@gmail.com");
+
+        // Setting the recipient's email address
+        message.setTo(toEmail);
+
+        // Setting the body of the email
+        message.setText(body);
+
+        // Setting the subject of the email
+        message.setSubject(subject);
+
+        // Sending the email using the JavaMailSender
+        mailSender.send(message);
+
+        System.out.println("Mail Sent Successfully...");
+    }
+}

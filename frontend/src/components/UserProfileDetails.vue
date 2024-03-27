bc50ae9cb283e099c1924813d038c0760737837f
Ismail El Kawwash <ismelk91@users.noreply.github.com>
2024-03-27 12:02:04 +0100

    Added design for UserProfileDetails

--- frontend/src/components/UserProfileDetails.vue
+++ frontend/src/components/UserProfileDetails.vue
@@ -1,42 +1,84 @@
 <template>
-    <div class="user-profile">
-        <h2>User Profile Page</h2>
+    <div v-if="user" class="user-profile-container">
 
-        <div v-if="user" class="profile-details">
-            <div>
-    <router-link to="/admin">Go back</router-link>
-                <h6 class="mb-0">First Name</h6>
-                <p class="text-secondary">{{ user.firstName }}</p>
-            </div>
-            <div>
-                <h6 class="mb-0">Last Name</h6>
-                <p class="text-secondary">{{ user.lastName }}</p>
-            </div>
-            <div>
-                <h6 class="mb-0">Birthdate</h6>
-                <p class="text-secondary">{{ user.birthDate }}</p>
-            </div>
-            <div>
-                <h6 class="mb-0">Email</h6>
-                <p class="text-secondary">{{ user.email }}</p>
-            </div>
-            <div>
-                <h6 class="mb-0">Phone Number</h6>
-                <p class="text-secondary">{{ user.phoneNumber }}</p>
+        <div class="top-left">
+            <router-link :to="goBackRoute">
+                <button class="back-btn btn btn-primary">Go back</button>
+            </router-link>
+        </div>
+        
+        <div class="content-container">
+
+            <label for="fileInput" class="img-container">
+                <img v-if="user.profilePictureUrl" :src="user.profilePictureUrl" :alt="user.firstName + user.lastName + ' Profile Picture'" />
+                <img v-else src="../assets/no-profile-picture.png" alt="No Profile Picture" />
+
+                <div class="user-name">
+                    {{ user.firstName }} {{ user.lastName }}
+                </div>
+            </label>
+
+            <div class="form-container">
+                <div class="row mb-3">
+                    <div class="label">
+                        <h6 class="mb-0">First name</h6>
+                    </div>
+                    <div class="value text-secondary">
+                        <input type="text" class="form-control" :value="user.firstName" disabled>
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <div class="label">
+                        <h6 class="mb-0">Last name</h6>
+                    </div>
+                    <div class="value text-secondary">
+                        <input type="text" class="form-control" :value="user.lastName" disabled>
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <div class="label">
+                        <h6 class="mb-0">Birthdate</h6>
+                    </div>
+                    <div class="value text-secondary">
+                        <input type="date" class="form-control" :value="user.birthDate" disabled>
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <div class="label">
+                        <h6 class="mb-0">Email</h6>
+                    </div>
+                    <div class="value text-secondary">
+                        <input type="text" class="form-control" :value="user.email" disabled>
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <div class="label">
+                        <h6 class="mb-0">Phone number</h6>
+                    </div>
+                    <div class="value text-secondary">
+                        <input type="text" class="form-control" :value="user.phoneNumber" disabled>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
 </template>
-
 <script>
-import {  ref, onMounted } from 'vue';
+import {  ref, onMounted, computed } from 'vue';
 import { fetchUserProfile } from '@/stores/user';
 import { useRoute } from 'vue-router';
+import { useAuthStore } from '@/stores/auth';
 
 export default {
     setup() {
         const userDetails = ref(null);
         const route = useRoute();
+        const authStore = useAuthStore();
 
         const fetchUserProfileData = async (email) => {
             try {
@@ -47,6 +89,10 @@ export default {
             }
         };
 
+        const goBackRoute = computed(() => {
+            return authStore.role === 'ROLE_ADMIN' ? '/admin' : '/colleagues';
+        });
+
         onMounted(() => {
             const email = route.params.email;
             fetchUserProfileData(email);
@@ -54,21 +100,91 @@ export default {
 
         return {
             user: userDetails,
+            goBackRoute,
         };
     },
 };
 </script>
 
 <style scoped>
-.user-profile {
-    max-width: 600px;
-    margin: auto;
+.user-profile-container {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 100px auto;
+    background-color: var(--blue);
+    color: var(--white);
+    width: 980px;
+    height: 500px;
+    border-radius: 9px;
+    padding: 20px;
 }
 
-.profile-details {
-    background-color: #f4f4f4;
-    padding: 20px;
-    border-radius: 8px;
-    margin-top: 20px;
+.top-left {
+    position: absolute;
+    top: 15px;
+    left: 15px;
+}
+
+.back-btn {
+    background-color: #223266;
+    color: var(--white);
+    padding: 6px 10px;
+    border: none;
+    font-size: 12px;
+    font-weight: 600;
+    letter-spacing: 1.5px;
+    text-transform: uppercase;
+}
+
+.back-btn:hover {
+    background-color: var(--orange);
+}
+
+.content-container {
+    display: flex;
+    width: 100%;
+}
+
+.img-container {
+    width: 30%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-right: 40px;
+}
+
+.img-container img {
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    margin-bottom: 30px;
+}
+
+.user-name {
+    font-size: 22px;
+    font-weight: 400;
+    letter-spacing: 1.5px;
+}
+
+.form-container {
+    flex: 1;
+    margin-left: 50px;
+    width: 70%;
+    text-transform: uppercase;
+}
+
+.form-control:disabled{
+    width: calc(100% - 40px);
+    padding: 5px 10px;
+    background-color: var(--blue);
+    border: 1px solid var(--white);
+    color: var(--white);
+}
+
+.label {
+    margin-bottom: 5px;
 }
 </style>
